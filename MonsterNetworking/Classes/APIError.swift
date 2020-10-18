6f76139429d8d16f1534948a4ab4b8eb6de293f8
印聪 <cong.yin@timanetworks.com>
2020-10-18 19:02:26 +0800

    修复APIError无妨访问成员变量的问题

--- MonsterNetworking/Classes/APIError.swift
+++ MonsterNetworking/Classes/APIError.swift
@@ -12,9 +12,9 @@ import Foundation
 public struct APIError: Error {
     var request: HTTPRequest
     
-    var code: Int = 0
-    var message: String = ""
-    var result: Any? = nil
+    public var code: Int = 0
+    public var message: String = ""
+    public var result: Any? = nil
     
     public init(request: HTTPRequest, code: NSInteger, message: String? = nil) {
         self.request = request
